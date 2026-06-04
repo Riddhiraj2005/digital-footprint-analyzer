3d04866996f4749a0b8ebb01cf7ca7c46e2555c7
Riddhi <rathoreriddhiraj@gmail.com>
2026-06-04 10:10:53 +0530

    Add start script for deployment

--- backend/server.js
+++ backend/server.js
@@ -397,6 +397,8 @@ app.get("/repos/:username", async (req, res) => {
     });
   }
 });
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
